933a76d66f6a5d6f5dc6768e45a539be54a48da6
jmgomezco <jmgomezco@gmail.com>
2026-01-05 03:26:59 +0100

    quitar cognito

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+/**
+ * Application Constants
+ * 
+ * This file contains all application-wide constant values.
+ * DO NOT modify these values without understanding the impact on API integration.
+ */
+
+/**
+ * Base URL for the CIE-10 API
+ * @type {string}
+ * @constant
+ */
+export const API_BASE_URL = 'https://fi8lp72ixb.execute-api.us-east-1.amazonaws.com/'
